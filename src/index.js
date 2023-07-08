601589b5d8418703a554e779998017016210a026
Nika Calapini <nmc2003@nmsu.edu>
2023-07-08 15:17:02 -0700

    update files for new tab

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import initialPageLoad from "./initial-page-load"
 import menu from "./menu";
+import contact from "./contact";
 
-console.log('hello')
 initialPageLoad();
 
 let tabSwitchingModule = (function(){
@@ -10,4 +10,7 @@ let tabSwitchingModule = (function(){
 
     const menuTab = document.querySelector(".menu");
     menuTab.addEventListener('click', menu);
+
+    const contactTab = document.querySelector(".contact");
+    contactTab.addEventListener('click', contact);
 })();
